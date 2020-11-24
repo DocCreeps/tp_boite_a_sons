3c2670908c7d2035fa77a791dfe484cd6d5dafe1
DocCreeps <dorian.vericel0546@gmail.com>
2020-11-24 14:54:29 +0100

    init tp boite a son c#
    tp boite a son cours RPI c#

---
+++ Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace VERICEL_Dorian
+{
+    partial class MusicBox
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Title = new System.Windows.Forms.Label();
+            this.ButtonSoundA = new System.Windows.Forms.Button();
+            this.ButtonSoundZ = new System.Windows.Forms.Button();
+            this.ButtonSoundE = new System.Windows.Forms.Button();
+            this.ButtonSoundR = new System.Windows.Forms.Button();
+            this.ButtonSoundT = new System.Windows.Forms.Button();
+            this.ButtonSoundQ = new System.Windows.Forms.Button();
+            this.ButtonSoundS = new System.Windows.Forms.Button();
+            this.ButtonSoundD = new System.Windows.Forms.Button();
+            this.ButtonSoundF = new System.Windows.Forms.Button();
+            this.ButtonSoundG = new System.Windows.Forms.Button();
+            this.ButtonSoundW = new System.Windows.Forms.Button();
+            this.ButtonSoundX = new System.Windows.Forms.Button();
+            this.ButtonSoundC = new System.Windows.Forms.Button();
+            this.ButtonSoundV = new System.Windows.Forms.Button();
+            this.ButtonSoundB = new System.Windows.Forms.Button();
+            this.PlaySound = new System.Windows.Forms.Button();
+            this.RecordSounds = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(296, 9);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(117, 24);
+            this.Title.TabIndex = 15;
+            this.Title.Text = "Boîte à sons ";
+            // 
+            // ButtonSoundA
+            // 
+            this.ButtonSoundA.Location = new System.Drawing.Point(32, 84);
+            this.ButtonSoundA.Name = "ButtonSoundA";
+            this.ButtonSoundA.Size = new System.Drawing.Size(75, 68);
+            this.ButtonSoundA.TabIndex = 0;
+            this.ButtonSoundA.Text = "A";
+            this.ButtonSoundA.UseVisualStyleBackColor = true;
+            this.ButtonSoundA.Click += new System.EventHandler(this.ButtonSoundA_Click);
+            this.ButtonSoundA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ButtonSoundA_KeyPress);
+            // 
+            // ButtonSoundZ
+            // 
+            this.ButtonSoundZ.Location = new System.Drawing.Point(176, 84);
+            this.ButtonSoundZ.Name = "ButtonSoundZ";
+            this.ButtonSoundZ.Size = new System.Drawing.Size(75, 68);
+            this.ButtonSoundZ.TabIndex = 1;
+            this.ButtonSoundZ.Text = "Z";
+            this.ButtonSoundZ.UseVisualStyleBackColor = true;
+            this.ButtonSoundZ.Click += new System.EventHandler(this.ButtonSoundZ_Click);
+            this.ButtonSoundZ.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ButtonSoundZ_KeyPress);
+            // 
+            // ButtonSoundE
+            // 
+            this.ButtonSoundE.Location = new System.Drawing.Point(317, 84);
+            this.ButtonSoundE.Name = "ButtonSoundE";
+            this.ButtonSoundE.Size = new System.Drawing.Size(75, 68);
+            this.ButtonSoundE.TabIndex = 2;
+            this.ButtonSoundE.Text = "E";
+            this.ButtonSoundE.UseVisualStyleBackColor = true;
+            this.ButtonSoundE.Click += new System.EventHandler(this.ButtonSoundE_Click);
+            this.ButtonSoundE.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ButtonSoundE_KeyPress);
+            // 
+            // ButtonSoundR
+            // 
+            this.ButtonSoundR.Location = new System.Drawing.Point(467, 84);
+            this.ButtonSoundR.Name = "ButtonSoundR";
+            this.ButtonSoundR.Size = new System.Drawing.Size(75, 68);
+            this.ButtonSoundR.TabIndex = 3;
+            this.ButtonSoundR.Text = "R";
+            this.ButtonSoundR.UseVisualStyleBackColor = true;
+            this.ButtonSoundR.Click += new System.EventHandler(this.ButtonSoundR_Click);
+            this.ButtonSoundR.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ButtonSoundR_KeyPress);
+            // 
+            // ButtonSoundT
+            // 
+            this.ButtonSoundT.Location = new System.Drawing.Point(622, 84);
+            this.ButtonSoundT.Name = "ButtonSoundT";
+            this.ButtonSoundT.Size = new System.Drawing.Size(75, 68);
+            this.ButtonSoundT.TabIndex = 4;
+            this.ButtonSoundT.Text = "T";
+            this.ButtonSoundT.UseVisualStyleBackColor = true;
+            this.ButtonSoundT.Click += new System.EventHandler(this.ButtonSoundT_Click);
+            this.ButtonSoundT.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ButtonSoundT_KeyPress);
+            // 
+            // ButtonSoundQ
+            // 
+            this.ButtonSoundQ.Location = new System.Drawing.Point(32, 180);
+            this.ButtonSoundQ.Name = "ButtonSoundQ";
+            this.ButtonSoundQ.Size = new System.Drawing.Size(75, 68);
+            this.ButtonSoundQ.TabIndex = 5;
+            this.ButtonSoundQ.Text = "Q";
+            this.ButtonSoundQ.UseVisualStyleBackColor = true;
+            this.ButtonSoundQ.Click += new System.EventHandler(this.ButtonSoundQ_Click);
+            this.ButtonSoundQ.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ButtonSoundQ_KeyPress);
+            // 
+            // ButtonSoundS
+            // 
+            this.ButtonSoundS.Location = new System.Drawing.Point(176, 180);
+            this.ButtonSoundS.Name = "ButtonSoundS";
+            this.ButtonSoundS.Size = new System.Drawing.Size(75, 68);
+            this.ButtonSoundS.TabIndex = 6;
+            this.ButtonSoundS.Text = "S";
+            this.ButtonSoundS.UseVisualStyleBackColor = true;
+            this.ButtonSoundS.Click += new System.EventHandler(this.ButtonSoundS_Click);
+            this.ButtonSoundS.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ButtonSoundS_KeyPress);
+            // 
+            // ButtonSoundD
+            // 
+            this.ButtonSoundD.Location = new System.Drawing.Point(317, 180);
+            this.ButtonSoundD.Name = "ButtonSoundD";
+            this.ButtonSoundD.Size = new System.Drawing.Size(75, 68);
+            this.ButtonSoundD.TabIndex = 7;
+            this.ButtonSoundD.Text = "D";
+            this.ButtonSoundD.UseVisualStyleBackColor = true;
+            this.ButtonSoundD.Click += new System.EventHandler(this.ButtonSoundD_Click);
+            this.ButtonSoundD.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ButtonSoundD_KeyPress);
+            // 
+            // ButtonSoundF
+            // 
+            this.ButtonSoundF.Location = new System.Drawing.Point(467, 180);
+            this.ButtonSoundF.Name = "ButtonSoundF";
+            this.ButtonSoundF.Size = new System.Drawing.Size(75, 68);
+            this.ButtonSoundF.TabIndex = 8;
+            this.ButtonSoundF.Text = "F";
+            this.ButtonSoundF.UseVisualStyleBackColor = true;
+            this.ButtonSoundF.Click += new System.EventHandler(this.ButtonSoundF_Click);
+            this.ButtonSoundF.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ButtonSoundF_KeyPress);
+            // 
+            // ButtonSoundG
+            // 
+            this.ButtonSoundG.Location = new System.Drawing.Point(622, 180);
+            this.ButtonSoundG.Name = "ButtonSoundG";
+            this.ButtonSoundG.Size = new System.Drawing.Size(75, 68);
+            this.ButtonSoundG.TabIndex = 9;
+            this.ButtonSoundG.Text = "G";
+            this.ButtonSoundG.UseVisualStyleBackColor = true;
+            this.ButtonSoundG.Click += new System.EventHandler(this.ButtonSoundG_Click);
+            this.ButtonSoundG.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ButtonSoundG_KeyPress);
+            // 
+            // ButtonSoundW
+            // 
+            this.ButtonSoundW.Location = new System.Drawing.Point(32, 276);
+            this.ButtonSoundW.Name = "ButtonSoundW";
+            this.ButtonSoundW.Size = new System.Drawing.Size(75, 68);
+            this.ButtonSoundW.TabIndex = 10;
+            this.ButtonSoundW.Text = "W";
+            this.ButtonSoundW.UseVisualStyleBackColor = true;
+            this.ButtonSoundW.Click += new System.EventHandler(this.ButtonSoundW_Click);
+            this.ButtonSoundW.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ButtonSoundW_KeyPress);
+            // 
+            // ButtonSoundX
+            // 
+            this.ButtonSoundX.Location = new System.Drawing.Point(176, 276);
+            this.ButtonSoundX.Name = "ButtonSoundX";
+            this.ButtonSoundX.Size = new System.Drawing.Size(75, 68);
+            this.ButtonSoundX.TabIndex = 11;
+            this.ButtonSoundX.Text = "X";
+            this.ButtonSoundX.UseVisualStyleBackColor = true;
+            this.ButtonSoundX.Click += new System.EventHandler(this.ButtonSoundX_Click);
+            this.ButtonSoundX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ButtonSoundX_KeyPress);
+            // 
+            // ButtonSoundC
+            // 
+            this.ButtonSoundC.Location = new System.Drawing.Point(317, 276);
+            this.ButtonSoundC.Name = "ButtonSoundC";
+            this.ButtonSoundC.Size = new System.Drawing.Size(75, 68);
+            this.ButtonSoundC.TabIndex = 12;
+            this.ButtonSoundC.Text = "C";
+            this.ButtonSoundC.UseVisualStyleBackColor = true;
+            this.ButtonSoundC.Click += new System.EventHandler(this.ButtonSoundC_Click);
+            this.ButtonSoundC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ButtonSoundC_KeyPress);
+            // 
+            // ButtonSoundV
+            // 
+            this.ButtonSoundV.Location = new System.Drawing.Point(467, 276);
+            this.ButtonSoundV.Name = "ButtonSoundV";
+            this.ButtonSoundV.Size = new System.Drawing.Size(75, 68);
+            this.ButtonSoundV.TabIndex = 13;
+            this.ButtonSoundV.Text = "V";
+            this.ButtonSoundV.UseVisualStyleBackColor = true;
+            this.ButtonSoundV.Click += new System.EventHandler(this.ButtonSoundV_Click);
+            this.ButtonSoundV.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ButtonSoundV_KeyPress);
+            // 
+            // ButtonSoundB
+            // 
+            this.ButtonSoundB.Location = new System.Drawing.Point(622, 276);
+            this.ButtonSoundB.Name = "ButtonSoundB";
+            this.ButtonSoundB.Size = new System.Drawing.Size(75, 68);
+            this.ButtonSoundB.TabIndex = 14;
+            this.ButtonSoundB.Text = "B";
+            this.ButtonSoundB.UseVisualStyleBackColor = true;
+            this.ButtonSoundB.Click += new System.EventHandler(this.ButtonSoundB_Click);
+            this.ButtonSoundB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ButtonSoundB_KeyPress);
+            // 
+            // PlaySound
+            // 
+            this.PlaySound.Location = new System.Drawing.Point(501, 398);
+            this.PlaySound.Name = "PlaySound";
+            this.PlaySound.Size = new System.Drawing.Size(121, 33);
+            this.PlaySound.TabIndex = 17;
+            this.PlaySound.Text = "Jouer les sons";
+            this.PlaySound.UseVisualStyleBackColor = true;
+            this.PlaySound.Click += new System.EventHandler(this.PlaySound_Click);
+            // 
+            // RecordSounds
+            // 
+            this.RecordSounds.Location = new System.Drawing.Point(130, 398);
+            this.RecordSounds.Name = "RecordSounds";
+            this.RecordSounds.Size = new System.Drawing.Size(121, 33);
+            this.RecordSounds.TabIndex = 19;
+            this.RecordSounds.Text = "Enregister";
+            this.RecordSounds.UseVisualStyleBackColor = true;
+            this.RecordSounds.Click += new System.EventHandler(this.RecordSounds_Click);
+            // 
+            // MusicBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 495);
+            this.Controls.Add(this.RecordSounds);
+            this.Controls.Add(this.PlaySound);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.ButtonSoundB);
+            this.Controls.Add(this.ButtonSoundV);
+            this.Controls.Add(this.ButtonSoundC);
+            this.Controls.Add(this.ButtonSoundX);
+            this.Controls.Add(this.ButtonSoundW);
+            this.Controls.Add(this.ButtonSoundG);
+            this.Controls.Add(this.ButtonSoundF);
+            this.Controls.Add(this.ButtonSoundD);
+            this.Controls.Add(this.ButtonSoundS);
+            this.Controls.Add(this.ButtonSoundQ);
+            this.Controls.Add(this.ButtonSoundT);
+            this.Controls.Add(this.ButtonSoundR);
+            this.Controls.Add(this.ButtonSoundE);
+            this.Controls.Add(this.ButtonSoundZ);
+            this.Controls.Add(this.ButtonSoundA);
+            this.Name = "MusicBox";
+            this.Text = "Boîte à music";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MusicBox_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonSoundA;
+        private System.Windows.Forms.Button ButtonSoundZ;
+        private System.Windows.Forms.Button ButtonSoundE;
+        private System.Windows.Forms.Button ButtonSoundR;
+        private System.Windows.Forms.Button ButtonSoundT;
+        private System.Windows.Forms.Button ButtonSoundQ;
+        private System.Windows.Forms.Button ButtonSoundS;
+        private System.Windows.Forms.Button ButtonSoundD;
+        private System.Windows.Forms.Button ButtonSoundF;
+        private System.Windows.Forms.Button ButtonSoundG;
+        private System.Windows.Forms.Button ButtonSoundW;
+        private System.Windows.Forms.Button ButtonSoundX;
+        private System.Windows.Forms.Button ButtonSoundC;
+        private System.Windows.Forms.Button ButtonSoundV;
+        private System.Windows.Forms.Button ButtonSoundB;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Button PlaySound;
+        private System.Windows.Forms.Button RecordSounds;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using NAudio.Wave;
+using NAudio;
+
+
+
+namespace VERICEL_Dorian
+{
+  
+    public partial class MusicBox : Form
+    {
+       
+        public MusicBox()
+        {
+            InitializeComponent();
+        }
+       
+        //Methode pour changer le chemin absolue => chemin relatif 
+        public static string Redirect(string redirection)
+        {
+            var currentDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent?.FullName;
+            string ressourcesPath = Path.Combine(currentDir, "ressources");
+            ressourcesPath = Path.Combine(currentDir, "ressources");
+            return Path.Combine(ressourcesPath, redirection);
+        }
+        //Déclaration des sons de l'aplication
+        WaveOutEvent waveOut = new WaveOutEvent();
+        
+        WaveFileReader SongA = new WaveFileReader(Redirect("bdrum.wav"));
+        WaveFileReader SongZ = new WaveFileReader(Redirect("bell.wav"));
+        WaveFileReader SongE = new WaveFileReader(Redirect("clap.wav"));
+        WaveFileReader SongR = new WaveFileReader(Redirect("click.wav"));
+        WaveFileReader SongT = new WaveFileReader(Redirect("Clikkick.wav"));
+        WaveFileReader SongG = new WaveFileReader(Redirect("dbass.wav"));
+        WaveFileReader SongF = new WaveFileReader(Redirect("flute.wav"));
+        WaveFileReader SongD = new WaveFileReader(Redirect("guitar.wav"));
+        WaveFileReader SongS = new WaveFileReader(Redirect("harp.wav"));
+        WaveFileReader SongQ = new WaveFileReader(Redirect("icechime.wav"));
+        WaveFileReader SongW = new WaveFileReader(Redirect("Kick.wav"));
+        WaveFileReader SongX = new WaveFileReader(Redirect("pling.wav"));
+        WaveFileReader SongC = new WaveFileReader(Redirect("sdrum.wav"));
+        WaveFileReader SongV = new WaveFileReader(Redirect("Tap.wav"));
+        WaveFileReader SongB = new WaveFileReader(Redirect("xylobone.wav"));
+       
+        //liste de son 
+        List<WaveFileReader> SongList = new List<WaveFileReader>();
+        //Méthode pour jouer un son
+        private void Playsong(WaveFileReader song)
+        {
+            waveOut.Init(song);
+            waveOut.Play();
+            song.Position = 0;
+        }
+        //Méthode qui verifie quelle touche en maj en apuyée 
+        private void KeyChecked(object sender, KeyPressEventArgs e)
+        {
+
+            if (e.KeyChar == (char)Keys.A)
+            {
+                waveOut.Init(SongA);
+                waveOut.Play();
+                SongA.Position = 0;
+            }
+            if (e.KeyChar == (char)Keys.Z)
+            {
+                waveOut.Init(SongZ);
+                waveOut.Play();
+                SongZ.Position = 0;
+            }
+            if (e.KeyChar == (char)Keys.E)
+            {
+                waveOut.Init(SongE);
+                waveOut.Play();
+                SongE.Position = 0;
+            }
+            if (e.KeyChar == (char)Keys.R)
+            {
+                waveOut.Init(SongR);
+                waveOut.Play();
+                SongR.Position = 0;
+            }
+            if (e.KeyChar == (char)Keys.T)
+            {
+                waveOut.Init(SongT);
+                waveOut.Play();
+                SongT.Position = 0;
+            }
+            if (e.KeyChar == (char)Keys.Q)
+            {
+                waveOut.Init(SongQ);
+                waveOut.Play();
+                SongQ.Position = 0;
+            }
+            if (e.KeyChar == (char)Keys.S)
+            {
+                waveOut.Init(SongS);
+                waveOut.Play();
+                SongS.Position = 0;
+            }
+            if (e.KeyChar == (char)Keys.D)
+            {
+                waveOut.Init(SongD);
+                waveOut.Play();
+                SongD.Position = 0;
+            }
+            if (e.KeyChar == (char)Keys.F)
+            {
+                waveOut.Init(SongF);
+                waveOut.Play();
+                SongF.Position = 0;
+            }
+            if (e.KeyChar == (char)Keys.G)
+            {
+                waveOut.Init(SongG);
+                waveOut.Play();
+                SongG.Position = 0;
+            }
+            if (e.KeyChar == (char)Keys.W)
+            {
+                waveOut.Init(SongW);
+                waveOut.Play();
+                SongW.Position = 0;
+            }
+            if (e.KeyChar == (char)Keys.X)
+            {
+                waveOut.Init(SongX);
+                waveOut.Play();
+                SongX.Position = 0;
+            }
+            if (e.KeyChar == (char)Keys.C)
+            {
+                waveOut.Init(SongC);
+                waveOut.Play();
+                SongC.Position = 0;
+            }
+            if (e.KeyChar == (char)Keys.V)
+            {
+                waveOut.Init(SongV);
+                waveOut.Play();
+                SongV.Position = 0;
+            }
+            if (e.KeyChar == (char)Keys.B)
+            {
+                waveOut.Init(SongB);
+                waveOut.Play();
+                SongB.Position = 0;
+            }
+        }
+        //sons au click sur le bouttonA
+        private void ButtonSoundA_Click(object sender, EventArgs e)
+        {
+            Playsong(SongA);
+        }
+        //sons      au click sur le buttonZ
+        private void ButtonSoundZ_Click(object sender, EventArgs e)
+        {
+            Playsong(SongZ);
+        }
+        //sons      au click sur le buttonE 
+        private void ButtonSoundE_Click(object sender, EventArgs e)
+        {
+            Playsong(SongE);
+        }
+        //sons      au click sur le buttonR 
+        private void ButtonSoundR_Click(object sender, EventArgs e)
+        {
+            Playsong(SongR);
+        }
+        //sons      au click sur le buttonT
+        private void ButtonSoundT_Click(object sender, EventArgs e)
+        {
+            Playsong(SongT);
+        }
+        //sons      au click sur le buttonG
+        private void ButtonSoundG_Click(object sender, EventArgs e)
+        {
+            Playsong(SongG);
+        }
+        //sons      au click sur le buttonB
+        private void ButtonSoundB_Click(object sender, EventArgs e)
+        {
+            Playsong(SongB);
+        }
+       
+        //sons      au click sur le buttonV
+        private void ButtonSoundV_Click(object sender, EventArgs e)
+        {
+            Playsong(SongV);
+        }
+        //sons      au click sur le buttonF
+        private void ButtonSoundF_Click(object sender, EventArgs e)
+        {
+            Playsong(SongF);
+        }
+        //sons      au click sur le buttonC 
+        private void ButtonSoundC_Click(object sender, EventArgs e)
+        {
+            Playsong(SongC);
+        }
+        //sons      au click sur le buttonD
+        private void ButtonSoundD_Click(object sender, EventArgs e)
+        {
+            Playsong(SongD);
+        }
+        //sons      au click sur le buttonS 
+        private void ButtonSoundS_Click(object sender, EventArgs e)
+        {
+            Playsong(SongS);
+        }
+        //sons      au click sur le buttonQ 
+        private void ButtonSoundQ_Click(object sender, EventArgs e)
+        {
+            Playsong(SongQ);
+        }
+        //sons      au click sur le buttonW
+        private void ButtonSoundW_Click(object sender, EventArgs e)
+        {
+            Playsong(SongW);
+        }
+        //sons      au click sur le buttonX 
+        private void ButtonSoundX_Click(object sender, EventArgs e)
+        {
+            Playsong(SongX);
+        }
+
+        //Button pour enregistrer une liste de sons
+        private void RecordSounds_Click(object sender, EventArgs e)
+        {
+            //création du fichier d'enregistrement 
+            var outputFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Enregistrement_Naudio");
+            Directory.CreateDirectory(outputFolder);
+            var outputFilePath = Path.Combine(outputFolder, "Enregistrement.wav");
+            //création de l'evenement d'enregistrement
+            var waveIn = new WaveInEvent();
+            WaveFileWriter writer = null;
+            writer = new WaveFileWriter(outputFilePath, waveIn.WaveFormat);
+            waveIn.StartRecording();
+
+
+            
+        }
+        //Button pour jouer la liste de sons
+        private void PlaySound_Click(object sender, EventArgs e)
+        {
+           /* waveOut.Init(writer);
+            waveOut.Play();
+           writer.Position = 0;*/
+        }
+
+        //a l'appui sur la touche A en majuscule 
+        private void ButtonSoundA_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            KeyChecked(sender, e);           
+        }
+        //a l'appui sur la touche Z en majuscule
+        private void ButtonSoundZ_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            KeyChecked(sender, e);
+        }
+        //a l'appui sur la touche E en majuscule
+        private void ButtonSoundE_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            KeyChecked(sender, e);
+        }
+        //a l'appui sur la touche R en majuscule
+        private void ButtonSoundR_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            KeyChecked(sender, e);
+        }
+        //a l'appui sur la touche T en majuscule
+        private void ButtonSoundT_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            KeyChecked(sender, e);
+        }
+        //a l'appui sur la touche Q en majuscule
+        private void ButtonSoundQ_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            KeyChecked(sender, e);
+        }
+        //a l'appui sur la touche S en majuscule
+        private void ButtonSoundS_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            KeyChecked(sender, e);
+        }
+        //a l'appui sur la touche D en majuscule
+        private void ButtonSoundD_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            KeyChecked(sender, e);
+        }
+        //a l'appui sur la touche F en majuscule
+        private void ButtonSoundF_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            KeyChecked(sender, e);
+        }
+        //a l'appui sur la touche G en majuscule
+        private void ButtonSoundG_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            KeyChecked(sender, e);
+        }
+        //a l'appui sur la touche W en majuscule
+        private void ButtonSoundW_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            KeyChecked(sender, e);
+        }
+        //a l'appui sur la touche X en majuscule
+        private void ButtonSoundX_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            KeyChecked(sender, e);
+        }
+        //a l'appui sur la touche C en majuscule
+        private void ButtonSoundC_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            KeyChecked(sender, e);
+        }
+        //a l'appui sur la touche V en majuscule
+        private void ButtonSoundV_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            KeyChecked(sender, e);
+        }
+        //a l'appui sur la touche B en majuscule
+        private void ButtonSoundB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            KeyChecked(sender, e);
+        }       
+        //evenement dans l'application 
+        private void MusicBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            KeyChecked(sender,e);
+        }
+        
+       
+    }
+
+
+
+   
+
+}
